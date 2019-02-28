77a687391135446cd6a20bdfd03f0cddcb446e64
andluo <1455234749@qq.com>
2019-02-28 22:05:28 +0800

    完成整体功能

--- js/index.js
+++ js/index.js
@@ -12,15 +12,129 @@ let EventCenter = {
 
 
 
-let App = {
+let Fm = {
     init:function(){
+        this.$container = $('#page-music main')
+        this.audio = new Audio()
+        this.audio.autoplay = true
         this.bind()
     },
 
     bind:function(){
-        EventCenter.on('channel_change',function(e,data){
-            console.log(data)
+
+        let _this = this
+
+        EventCenter.on('channel_change',(e,channel_obj)=>{
+            this.channel_id = channel_obj.channel_id
+            this.channel_name = channel_obj.channel_name
+            this.load_music(this.set_music)
+
         })
+
+        //播放/暂停
+        this.$container.find('.btn-play').on('click',function(){
+            if($(this).hasClass('icon-play')){
+                $(this).removeClass('icon-play').addClass('icon-pause')
+                _this.audio.play()
+            }else{
+                $(this).removeClass('icon-pause').addClass('icon-play')
+                _this.audio.pause()
+            }
+        })
+
+        //下一首
+        this.$container.find('.btn-next').on('click',function(){
+            _this.load_music(_this.set_music)
+        })
+
+        this.audio.addEventListener('play',function(){
+            clearInterval(_this.audio_clock)
+            _this.audio_clock = setInterval(function(){
+                update_status()
+            },1000)
+        })
+
+        this.audio.addEventListener('pause',function(){
+            clearInterval(_this.audio_clock)
+            console.log("暂停中...")
+            
+        })
+
+        function update_status(){
+            let min = Math.floor(_this.audio.currentTime/60)
+            let second = Math.floor(_this.audio.currentTime%60)+''
+            second = second.length === 2 ? second:0+second
+            let currentTime = `${min}:${second}`
+            _this.$container.find('.current-time').text(currentTime)
+            _this.$container.find('.bar-progress').css({width:_this.audio.currentTime*100/_this.audio.duration+'%'})
+            console.log(_this.lyric_obj[0+currentTime])
+            let current_lyric = _this.lyric_obj[0+currentTime]
+            if(current_lyric){
+                _this.$container.find('.lyric p').text(current_lyric)
+
+            }
+        }
+
+        
+    },
+
+
+    load_music:function(set_music){
+        console.log("load music...")
+        $.getJSON("//api.jirengu.com/fm/getSong.php",{channel:this.channel_id})
+         .done((res)=>{
+            console.log("加载音乐成功.")
+            console.log(res)
+            this.song = res.song[0]
+            
+            set_music.call(this)
+         })
+         .fail(function(){
+             console.log("加载音乐失败.")
+         })
+    },
+
+    set_music:function(){
+        this.audio.src = this.song.url
+        console.log("开始设置音乐.")
+        $('.bg').css({'background-image':`url(${this.song.picture})`})
+        this.$container.find('figure').css({'background-image':`url(${this.song.picture})`})
+        this.$container.find('.detail h1').text(this.song.title)
+        this.$container.find('.tag').text(this.channel_name)
+        this.$container.find('.author').text(this.song.artist)
+        this.$container.find('.btn-play').removeClass('.icon-play').addClass('.icon-pause')
+        this.load_lyric(this.song.sid)
+    },
+
+    load_lyric:function(sid){
+        let _this = this
+        $.getJSON('//jirenguapi.applinzi.com/fm/getLyric.php',{sid:sid})
+         .done(function(res){
+             console.log("获取歌词成功...")
+             console.log(res)
+             _this.lyric_obj = {}
+             let lyric = res.lyric
+             
+             let times = null
+             let  str = null
+             lyric.split('\n').forEach((line)=>{
+                 times = line.match(/\d{2}:\d{2}/g)
+                 console.log(times)
+                 str = line.replace(/\[.+?\]/g,'')
+                 if(Array.isArray(times)){
+                    times.forEach((time)=>{
+                        _this.lyric_obj[time] = str
+                    })
+                 }
+                 
+             })
+             
+
+         })
+         .fail(function(res){
+            console.log("获取歌词失败...")
+            console.log(res)
+         })
     }
 }
 
@@ -81,7 +195,14 @@ let  Footer = {
 
         this.$ul.on('click','li',function(e){
             let channel_id = e.currentTarget.dataset.channelId
-            EventCenter.fire('channel_change',channel_id)
+            let channel_name = e.currentTarget.dataset.channelName
+            if(!$(this).hasClass('active')){
+                $(this).addClass('active').siblings().removeClass('active')
+            }
+            EventCenter.fire('channel_change',
+                            {channel_id:channel_id,
+                             channel_name:channel_name}
+                            )
 
         })
         
@@ -117,15 +238,12 @@ let  Footer = {
     setStyle:function(){
         let li_widht = this.$footer.find('li').outerWidth(true)
         let li_count = this.$footer.find('li').length
-        console.log(`总共有${li_count}个li`)
 
         let ul_width = Math.ceil(li_count * li_widht)
         let box_width = this.$box.width()
 
         let per_page_li = (Math.floor(box_width/li_widht))
-        console.log(`每页${per_page_li}个li`)
         this.$foot_page_count = Math.ceil(li_count/per_page_li)
-        console.log(`总共${this.$foot_page_count}页`)
 
 
         this.$footer.find('ul').css({width:ul_width})
@@ -133,5 +251,5 @@ let  Footer = {
 }
 
 Footer.init()
-App.init()
+Fm.init()
 
